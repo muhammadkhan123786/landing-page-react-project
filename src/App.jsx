e94fd8c1d6a2f4026fad66c10305cab54cc70ebc
muhammadkhan123786 <muhammadkhan123829@gmail.com>
2025-08-01 16:21:47 +0500

    � Added responsive Hero section with desktop and mobile optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import HeroSection from './components/hero/HeroSection';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
-    <>
-      <Navbar />
-    </>
+    <main className="relative min-h-screen overflow-x-hidden">
+      <div className="absolute -top-28 -left-28 w-[500px] h-[500px] bg-gradient-to-tr from-indigo-500/20 to-pink-500/20 rounded-full blur-[80px] -z-10"></div>
+      <div className="overflow-hidden">
+        <Navbar />
+        <HeroSection />
+      </div>
+    </main>
   );
 }
 
---
+++ src/components/hero/HeroSection.jsx
@@ -0,0 +1,50 @@
+import { hero } from './hero';
+import { header } from '../navbar/navbarStyles';
+import heroimage from '../../assets/hero-image.png';
+const HeroSection = () => {
+  return (
+    <section className={hero.section}>
+      <div className={hero.left.leftParentDiv}>
+        {/*left section */}
+        <div className={hero.left.startdiv}>
+          <span className={hero.left.star}>⭐</span>
+          <span className={hero.left.text}>Jump start your growth</span>
+        </div>
+        <h1 className={hero.left.h1}>
+          We boost the growth for
+          <span className={hero.left.blue}>Startup to Fortune 500 </span>
+          <span className="inline-block">
+            Companies <span className="animate-pulse">⏰</span>
+          </span>
+        </h1>
+        <p className={hero.left.p}>
+          Get the most leads, sales people training and conversions, tools and
+          more - all within the same one billing.{' '}
+        </p>
+
+        <div className={hero.left.email_div.main}>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Enter email address"
+            className={hero.left.email_div.emailInput}
+          />
+          <button className={header.button + ' rounded-xl'}>→</button>
+        </div>
+      </div>
+      <div className={hero.right.main}>
+        {/*right section */}
+        <div className="relative">
+          <img
+            src={heroimage}
+            alt="hero section image"
+            className={hero.right.img}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
